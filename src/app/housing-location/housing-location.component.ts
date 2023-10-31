666a09c3e90661d56e055debc07dd0a23d403c8a
Justen Anderson <justenanderson@gmail.com>
2023-10-30 21:03:51 -0600

    Add dynamic inputs for listing heading and location, format with prettier

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,24 +1,30 @@
-import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HousingLocation } from '../housing-location';
+import { Component, Input } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { HousingLocation } from '../housing-location'
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule],
   template: `
-   <section class="listing">
-    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
-    <h2 class="listing-heading"></h2>
-    <p class="listing-location"></p> 
-   </section>
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+    </section>
   `,
-  styleUrls: ['./housing-location.component.css']
+  styleUrls: ['./housing-location.component.css'],
 })
 export class HousingLocationComponent {
   // @Input() is an input decorator, which requires the @ symbol prefix and the parens at the end
   // The exclamation point is the non-null assertion operator. It tells tsc that the value won't be null or undefined
-  @Input() housingLocation!:HousingLocation;
+  @Input() housingLocation!: HousingLocation
 }
 
 // Data is passed in components with @Input properties, allowing the data to flow from the parent to child element(s)
