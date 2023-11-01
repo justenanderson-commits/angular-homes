1f6ba9c78f00d5ec205881af56820d45ee1726e8
Justen Anderson <justenanderson@gmail.com>
2023-11-01 15:34:34 -0600

    Update housing-location component and routes for parameterized routing

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HousingLocation } from '../housing-location'
+import { RouterModule } from '@angular/router'
+
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
     <section class="listing">
       <img
@@ -17,6 +19,8 @@ import { HousingLocation } from '../housing-location'
       <p class="listing-location">
         {{ housingLocation.city }}, {{ housingLocation.state }}
       </p>
+      <!-- Square brackets around the routerLink directive is for property binding and it means that the right-hand side value with support dynamic values and values other than strings. -->
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
 
   `,
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -6,14 +6,16 @@ const routeConfig: Routes = [{
   path: '',
   component: HomeComponent,
   title: 'Home Page'
-}, {
-  path: 'details',
+}, 
+{
+  path: 'details/:id',
   component: DetailsComponent,
   title: "Details Page"
 }]
 
 export default routeConfig
 
-// Learning Notes
+// Learning Notes:
 // Routes is an array of objects
 // Each route is an object literal with the properties path, component, and title
+// The `:id` is a placeholder for parameterized routing
