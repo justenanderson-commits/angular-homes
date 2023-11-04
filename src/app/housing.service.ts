b7783341f3d0ac2e364bd0e5884a76b86ab9b151
Justen Anderson <justenanderson@gmail.com>
2023-11-04 15:03:48 -0600

    Fix url bug in housing.service.ts

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -5,7 +5,7 @@ import { HousingLocation } from './housing-location'
   providedIn: 'root',
 })
 export class HousingService {
-  url = 'http://localhost:300/locations'
+  url = 'http://localhost:3000/locations'
 
   constructor() {}
 
