80363a5d33cc0adf9cff59a1176dc9ee853f6661
Justen Anderson <justenanderson@gmail.com>
2023-11-11 08:28:37 -0600

    Add route for add-location component

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component'
 import { DetailsComponent } from './details/details.component'
+import { AddLocationComponent } from './add-location/add-location.component'
 
 const routeConfig: Routes = [{
   path: '',
@@ -11,6 +12,11 @@ const routeConfig: Routes = [{
   path: 'details/:id',
   component: DetailsComponent,
   title: "Details Page"
+},
+{
+  path: 'add-location',
+  component: AddLocationComponent,
+  title: "Add New Location"
 }]
 
 export default routeConfig
